959a0188adf58727e04754841f614f83c41b8e2e
Denis Bolshakov <Dysprosium92@ya.ru>
2021-11-29 15:54:23 +0300

    Functor interface implemented for Either container

--- src/Either.hs
+++ src/Either.hs
@@ -1,3 +1,7 @@
 import Prelude hiding (Either, Left, Right, Functor, fmap)
 
 data Either a b = Left a | Right b
+
+instance Functor Either e where
+  fmap _ (Left x) = Left x
+  fmap f (Right x) = Right (f x)
