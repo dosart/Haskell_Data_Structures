a378193968860f4f72da6f983a1349ccffe2f8b0
Denis Bolshakov <Dysprosium92@ya.ru>
2021-11-29 15:21:03 +0300

    Functor interface implemented for Maybe container

--- src/Maybe.hs
+++ src/Maybe.hs
@@ -1,3 +1,7 @@
 import Prelude hiding (Maybe, Just, Nothing, Functor, fmap)
 
 data Maybe a = Nothing | Just a
+
+instance Functor Maybe where
+  fmap _ Nothing = Nothing
+  fmap f (Just x) = Just (f x)
